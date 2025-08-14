d11078be84de6eb825476d74f9edd49992ae3cdc
xize <glucassen@live.nl>
2025-08-15 00:22:09 +0200

    initial commit

---
+++ pbr_line_split/Window.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace pbr_line_split
+{
+    partial class Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.policyfield = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.RichTextBox();
+            this.convertbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // policyfield
+            // 
+            this.policyfield.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.policyfield.Location = new System.Drawing.Point(107, 51);
+            this.policyfield.Name = "policyfield";
+            this.policyfield.Size = new System.Drawing.Size(99, 20);
+            this.policyfield.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "max spaces to split?";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(171, 11);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "original policy line:";
+            // 
+            // result
+            // 
+            this.result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.result.Location = new System.Drawing.Point(4, 132);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(202, 137);
+            this.result.TabIndex = 4;
+            this.result.Text = "";
+            // 
+            // convertbtn
+            // 
+            this.convertbtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.convertbtn.Location = new System.Drawing.Point(131, 86);
+            this.convertbtn.Name = "convertbtn";
+            this.convertbtn.Size = new System.Drawing.Size(75, 23);
+            this.convertbtn.TabIndex = 6;
+            this.convertbtn.Text = "convert";
+            this.convertbtn.UseVisualStyleBackColor = true;
+            this.convertbtn.Click += new System.EventHandler(this.convertbtn_Click);
+            // 
+            // Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 281);
+            this.Controls.Add(this.convertbtn);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.policyfield);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Window";
+            this.Text = "OpenWrt PBR splitter";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox policyfield;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox result;
+        private System.Windows.Forms.Button convertbtn;
+    }
+}
+
---
+++ pbr_line_split/Window.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pbr_line_split
+{
+    public partial class Window: Form
+    {
+        public Window()
+        {
+            InitializeComponent();
+        }
+
+        private void convertbtn_Click(object sender, EventArgs e)
+        {
+            this.convertbtn.Enabled = false;
+            int max = (int)this.numericUpDown1.Value;
+            string text = this.policyfield.Text;
+            if(!text.Contains(' '))
+            {
+                MessageBox.Show("Error: the policy does not contain spaces.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            //first count how many occurences we get by dividing all spaces by max.
+            int occurencies = text.Split(' ').Length;
+
+            if ((occurencies / max) <= 0) {
+                MessageBox.Show("Error: the policy is less than the maximum defined spaces.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Console.WriteLine("occurencies: " + occurencies/max);
+
+            string[] list = text.Split(' ');
+            int index = 0;
+            Console.WriteLine("max:"+max);
+            for(int occurence = 0; occurence <= list.Length/max-1; occurence++) {
+
+                Console.WriteLine("occurence: "+occurence);
+
+                StringBuilder build = new StringBuilder();
+                for(int i = 0; i < max; i++)
+                {
+                    Console.WriteLine("i:"+(i+index));
+                    build.Append(list[i + index]).Append(" ");   
+                }
+     
+
+                result.Text += "Policy #" + occurence + ": "+build.ToString().TrimEnd(' ')+"\n\n";
+                result.Update();
+                index+=max;
+            }
+
+            if (index < list.Length - 1)
+            {
+                //okay we add the remaining ones.
+                StringBuilder build = new StringBuilder();
+                for (int i = index; i <= list.Length-1; i++) {
+                    Console.WriteLine("i:" + i);
+                    build.Append(list[i]).Append(" ");
+                }
+                result.Text += "Policy #" + (occurencies / max+1) + ": " + build.ToString().TrimEnd(' ') + "\n\n";
+                result.Update();
+                index += max;
+            }
+
+            Console.WriteLine("size: "+(list.Length-1));
+
+            this.result.Update();
+            this.convertbtn.Enabled = true;
+        }
+    }
+}
